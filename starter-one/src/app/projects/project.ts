4f900c916714139d802f05f3ac33b19d3fd93387
Daniel Turcu <academmia@gmail.com>
2022-09-29 09:19:32 +0300

    '06_00W220929'

---
+++ starter-one/src/app/projects/project.ts
@@ -0,0 +1,13 @@
+
+export interface IProject {
+    id: number,
+    code: string,
+    name: string,
+    description: string,
+    owner_id: number,
+    owner_name: string,
+    category_id: number,
+    created_date: string,
+    end_date: string
+}
+
